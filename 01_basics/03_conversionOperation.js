8074f9a8118f9870ed004ada4b8e1dcef882750b
Ashutosh Naik <naikashu2401@gmail.com>
2023-09-23 20:42:36 +0530

    conversion and operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,7 +30,7 @@ let stringNumber = String(someNumber);
 
 let value = 3;
 let negValue = -value;
-// console.log(negValue);
+console.log(negValue);
 
 // console.log(2+2);
 // console.log(2-2);
@@ -42,7 +42,7 @@ let negValue = -value;
 let str1 = "hello";
 let str2 = " ashutosh";
 let str3 = str1 + str2;
-// console.log(str3);
+console.log(str3);
 
 // console.log("1" + 2);
 // console.log(1 + "2");
