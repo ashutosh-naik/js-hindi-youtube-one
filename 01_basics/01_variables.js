5a6b67307d688568e6a37be4f994377657f8a492
Ashutosh Naik <naikashu2401@gmail.com>
2023-09-23 20:03:46 +0530

    updated variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,17 +1,26 @@
-const accountID = 12345;
-let accountEmail = "ashutosh@gmail.com";
-var accountPassword = "45872";
-accountCity = "Pune";
+const accountId = 144553;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
 let accountState;
 
+// accountId = 2; // not allowed
+
+accountEmail = "hc@hc.com";
+accountPassword = "21212121";
+accountCity = "Pune";
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
 console.table([
-  accountID,
+  accountId,
   accountEmail,
   accountPassword,
   accountCity,
   accountState,
 ]);
-
-/* 
-prefer not to use var because of issue in block scope and functional scope
-*/
