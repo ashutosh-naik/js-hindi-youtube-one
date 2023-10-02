ff6f8254ecd1916d2524bc106ce547914bc896cd
Ashutosh Naik <naikashu2401@gmail.com>
2023-10-03 02:15:45 +0530

    switch in js

--- 04_control_flow/02_switch.js
+++ 04_control_flow/02_switch.js
@@ -26,3 +26,32 @@ switch (dayNumber) {
     console.log("Invalid day");
     break;
 }
+
+const dayName = "Monday";
+
+switch (dayName) {
+  case "Sunday":
+    console.log("Sunday");
+    break;
+  case "Monday":
+    console.log("Monday");
+    break;
+  case "Wednesday":
+    console.log("Tuesday");
+    break;
+  case "Wednesday":
+    console.log("Wednesday");
+    break;
+  case "Thursday":
+    console.log("Thursday");
+    break;
+  case "Friday":
+    console.log("Friday");
+    break;
+  case "Saturday":
+    console.log("Saturday");
+    break;
+  default:
+    console.log("Invalid day");
+    break;
+}
