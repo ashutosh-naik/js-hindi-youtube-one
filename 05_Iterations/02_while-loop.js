b517ba18e415d8edfa1e2bbe515506ba2ce04e15
Ashutosh Naik <naikashu2401@gmail.com>
2023-10-12 08:20:19 +0530

    while loop

---
+++ 05_Iterations/02_while-loop.js
@@ -0,0 +1,12 @@
+// let index = 0;
+// while (index <= 10) {
+//   console.log(`Value of index is ${index}`);
+//   index += 2;
+// }
+
+let myArr = ["Batman", "Superman", "Spiderman"];
+let arr = 0;
+while (arr < myArr.length) {
+  console.log(`Value is: ${myArr[arr]}`);
+  arr++;
+}
