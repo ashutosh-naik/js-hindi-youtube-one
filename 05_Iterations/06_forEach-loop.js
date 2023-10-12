f1ee8c4dcd08e34ff63d2225c6dee1260e2034d1
Ashutosh Naik <naikashu2401@gmail.com>
2023-10-12 10:19:23 +0530

    forEach loop

---
+++ 05_Iterations/06_forEach-loop.js
@@ -0,0 +1,37 @@
+const coding = ["java", "javascript", "python", "swift", "rust"];
+
+coding.forEach(function (value) {
+  console.log(value);
+});
+
+coding.forEach((item) => {
+  console.log(item);
+});
+
+function printMe(item) {
+  console.log(item);
+}
+coding.forEach(printMe);
+
+coding.forEach((item, index, arr) => {
+  console.log(item, index, arr);
+});
+
+const myCoding = [
+  {
+    languageName: "javascript",
+    languageFileName: "js",
+  },
+  {
+    languageName: "java",
+    languageFileName: "java",
+  },
+  {
+    languageName: "python",
+    languageFileName: "py",
+  },
+];
+myCoding.forEach((item) => {
+  console.log(item.languageName);
+  console.log(item.languageFileName);
+});
